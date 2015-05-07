32f760f62a0cb251916f45560c6b13fa2660a8d7
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-05-07 16:23:47 -0600

    Remote-triggered commit: 2015-0507-18:23:46:

--- main-file.php
+++ main-file.php
@@ -6,6 +6,11 @@
  */
 
 
+// For the sake of logical-instantanaety, Storikaze only retrieves
+// the date once per run.
+$storikaze_time_now = current_time('mysql');
+
+
 // WordPress was originally written for blogs - and therefore
 // sorts the entries so that the most recent one appears first.
 // However, that is not what we want in webfiction - so this
@@ -18,6 +23,44 @@ function storikaze_fix_sort_order_in_qry ( $query )
 }
 endif; // twentyfifteen_setup
 add_action( 'pre_get_posts', 'storikaze_fix_sort_order_in_qry' );
+
+
+// Just as with webcomics, the author of a webserial wants to
+// be able to schedule in-advance when a subchapter is added
+// to the book. This means two things (1) scheduling when the
+// post itself appears in the post-sequence and (2) scheduling
+// the Table of Contents to reflect the change as well. For
+// actual posts themselves (issue #1) WordPress has it's own
+// built-in scheduling feature. But for issue #2, you will need
+// a shortcode to enclose sections of text who's visibility is
+// scheculed. Introducing the [storikaze_at] shortcode.
+if ( ! function_exists( 'storikaze_tag_at' ) ) :
+function storikaze_tag_at ( $atts, $content = null ) {
+  $timecode = strtotime($GLOBALS["storikaze_time_now"]);
+  
+  foreach ( $atts as $thenom => $theval )
+  {
+    
+    if ( $thenom = "from" )
+    {
+      if ( $timecode < strtotime($theval) ) { return ""; }
+    }
+    
+    // The "to" attribute specifies a time at which the text
+    // disappears (such as the "story still in progress" note
+    // at the end of the TOC -- but only once you know at what
+    // time the very last installation of the story will be
+    // added).
+    if ( $thenom = "to" )
+    {
+      if ( $timecode > strtotime($theval) ) { return ""; }
+    }
+  }
+  
+  return $content;
+}
+endif;
+add_shortcode( 'storikaze_at', 'storikaze_tag_at' );
 
 
 
