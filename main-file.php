8c693a7589c7712004db7f14bd4bc4e4ff0b3b84
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-05-07 17:24:50 -0600

    Remote-triggered commit: 2015-0507-19:24:50:

--- main-file.php
+++ main-file.php
@@ -10,6 +10,17 @@
 // the date once per run.
 $storikaze_time_now = current_time('mysql');
 
+// And it is important for Storikaze to know whether or not we are
+// running in admin mode.
+$storikaze_admin_now = false;
+if ( ! function_exists( 'storikaze_upon_admin' ) ) :
+function storikaze_upon_admin ( )
+{
+	$GLOBALS["storikaze_admin_now"] = true;
+}
+endif;
+add_action( 'admin_init', 'storikaze_upon_admin' );
+
 
 // WordPress was originally written for blogs - and therefore
 // sorts the entries so that the most recent one appears first.
@@ -19,7 +30,10 @@ $storikaze_time_now = current_time('mysql');
 if ( ! function_exists( 'storikaze_fix_sort_order_in_qry' ) ) :
 function storikaze_fix_sort_order_in_qry ( $query )
 {
-	$query->set('order','ASC');
+	if ( ! $GLOBALS["storikaze_admin_now"] )
+	{
+		$query->set('order','ASC');
+	}
 }
 endif; // twentyfifteen_setup
 add_action( 'pre_get_posts', 'storikaze_fix_sort_order_in_qry' );
@@ -36,28 +50,28 @@ add_action( 'pre_get_posts', 'storikaze_fix_sort_order_in_qry' );
 // scheculed. Introducing the [storikaze_at] shortcode.
 if ( ! function_exists( 'storikaze_tag_at' ) ) :
 function storikaze_tag_at ( $atts, $content = null ) {
-  $timecode = strtotime($GLOBALS["storikaze_time_now"]);
-  
-  foreach ( $atts as $thenom => $theval )
-  {
-    
-    if ( $thenom = "from" )
-    {
-      if ( $timecode < strtotime($theval) ) { return ""; }
-    }
-    
-    // The "to" attribute specifies a time at which the text
-    // disappears (such as the "story still in progress" note
-    // at the end of the TOC -- but only once you know at what
-    // time the very last installation of the story will be
-    // added).
-    if ( $thenom = "to" )
-    {
-      if ( $timecode > strtotime($theval) ) { return ""; }
-    }
-  }
-  
-  return $content;
+	$timecode = strtotime($GLOBALS["storikaze_time_now"]);
+	
+	foreach ( $atts as $thenom => $theval )
+	{
+		
+		if ( $thenom = "from" )
+		{
+			if ( $timecode < strtotime($theval) ) { return ""; }
+		}
+		
+		// The "to" attribute specifies a time at which the text
+		// disappears (such as the "story still in progress" note
+		// at the end of the TOC -- but only once you know at what
+		// time the very last installation of the story will be
+		// added).
+		if ( $thenom = "to" )
+		{
+			if ( $timecode > strtotime($theval) ) { return ""; }
+		}
+	}
+	
+	return $content;
 }
 endif;
 add_shortcode( 'storikaze_at', 'storikaze_tag_at' );
