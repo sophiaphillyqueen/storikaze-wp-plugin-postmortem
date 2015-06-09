1458360385cae4b8e3f61f487f38cb18d2fbbce5
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-06-08 23:53:44 -0600

    Facebook and Twitter NOT aligned

--- work_add_social.php
+++ work_add_social.php
@@ -10,6 +10,10 @@ function storikaze_adcn_social_fun ( $content ) {
   $reto = "";
   $curloc = get_permalink();
   $clrc = htmlspecialchars($curloc);
+  $ttx_up = get_the_title();
+  $ttx_pr = htmlspecialchars($ttx_up);
+  $blgnm_up = get_bloginfo('name');
+  $blgnm_pr = htmlspecialchars($blgnm_up);
   
   if ( $GLOBALS['storikaze_adcn_social_first'] )
   {
@@ -26,11 +30,33 @@ function storikaze_adcn_social_fun ( $content ) {
   
   $reto .= $content;
   
+  
+  // Add Facebook Button
   $reto .= '<div class="fb-like" data-href="';
   $reto .= $clrc;
   $reto .= '" data-layout="button_count" data-action="like"';
   $reto .= ' data-show-faces="true" data-share="true"></div>';
   $reto .= "\n";
+  
+  // Add twitter button
+  // Used this site as resource: https://about.twitter.com/resources/buttons#tweet
+  $reto .= '<a href="https://twitter.com/share" class="twitter-share-button"';
+  $reto .= ' data-url="';
+  $reto .= $clrc;
+  $reto .= '" data-text="';
+  $reto .= $blgnm_pr;
+  $reto .= ': ';
+  $reto .= $ttx_pr;
+  $reto .= '"';
+  //$reto .= ' data-via="SophiannaGirl"';
+  $reto .= '>Tweet</a> <script>!function(d,s,id)';
+  $reto .= '{var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?';
+  $reto .= '\'http\':\'https\';if(!d.getElementById(id))';
+  $reto .= '{js=d.createElement(s);js.id=id;js.src=';
+  $reto .= 'p+\'://platform.twitter.com/widgets.js\';';
+  $reto .= 'fjs.parentNode.insertBefore(js,fjs);}}';
+  $reto .= '(document, \'script\', \'twitter-wjs\');';
+  $reto .= '</script>' . "\n";
   
   $GLOBALS['storikaze_adcn_social_first'] = false;
   return $reto;
